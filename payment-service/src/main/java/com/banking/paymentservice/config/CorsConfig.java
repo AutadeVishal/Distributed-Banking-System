ceec03c845d2058a190d5ae22f2df887468f000a
Vishal Autade <autadevishalvijay@gmail.com>
2026-09-15 15:02:35 +0530

    Cors Config for Payment Serivce

--- payment-service/src/main/java/com/banking/paymentservice/config/CorsConfig.java
+++ payment-service/src/main/java/com/banking/paymentservice/config/CorsConfig.java
@@ -1,4 +1,22 @@
 package com.banking.paymentservice.config;
 
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+    @Bean//this bean says put the returned object in container
+    public WebMvcConfigurer corsConfigure(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings( CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("GET","POST","PUT","DELETE");
+            }
+        };
+    }
 }
